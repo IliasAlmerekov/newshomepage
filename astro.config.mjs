927a6d7bc802aadd96497ce50355b9445ed5a208
Ilias Almerekov <ilias.almerekov@db-n.com>
2025-01-10 16:00:04 +0100

    changing

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,8 @@ import storyblok from "@storyblok/astro";
 const env = loadEnv("", process.cwd(), "STORYBLOK");
 import { loadEnv } from "vite";
 import react from "@astrojs/react";
+import basicSsl from "@vitejs/plugin-basic-ssl";
+import P1Article from "./src/templates/P1-Article.astro";
 
 // https://astro.build/config
 export default defineConfig({
@@ -17,9 +19,17 @@ export default defineConfig({
         stage: "components/Stage",
         newsTeaser: "components/Teaser",
         page: "templates/page",
-        Header: "components/Header",
+        Globals: "components/Header",
         articleTeaser: "components/ArticleTeaser",
+        article: "templates/P1-Article",
       },
     }),
   ],
+  vite: {
+    plugins: [basicSsl()],
+    server: {
+      // @ts-ignore
+      https: true,
+    },
+  },
 });
