80dd29dd5217c1f05b3ef86cd05e3c7ff0242824
Ilias Almerekov <ilias.almerekov@db-n.com>
2025-06-05 12:08:10 +0200

    chore: change the project architecture

---
+++ src/middleware.ts
@@ -0,0 +1,7 @@
+// src/middleware.ts
+import { defineMiddleware } from "astro:middleware";
+
+export const onRequest = defineMiddleware(async (context, next) => {
+  const response = await next();
+  return response;
+});
