4cf8a17e4288403b545186f938e2658743c97230
Ilias Almerekov <ilias.almerekov@db-n.com>
2025-06-05 12:17:31 +0200

    chore: add middleware

--- src/middleware.ts
+++
@@ -1,7 +0,0 @@
-// src/middleware.ts
-import { defineMiddleware } from "astro:middleware";
-
-export const onRequest = defineMiddleware(async (context, next) => {
-  const response = await next();
-  return response;
-});
